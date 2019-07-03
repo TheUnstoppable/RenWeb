6031cacdc614fbddfbb2a820538f62af620772ac
The Unstoppable <canozkan145@gmail.com>
2019-07-04 00:38:53 +0300

    Added Newtonsoft.JSON and Embedding partially done.

---
+++ RenWeb/EmbedClass.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenWeb
+{
+    public class RenderableEmbedClass
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string Name = null;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Width = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Height = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public string BackgroundFile = null;
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public RenderableEmbedTextClass[] Texts = new RenderableEmbedTextClass[0];
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public RenderableEmbedImageClass[] Images = new RenderableEmbedImageClass[0];
+    }
+
+    public class RenderableEmbedImageClass
+    {
+        [JsonProperty(Required = Required.Always)]
+        public int X = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Y = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Width = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Height = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public string Source = null;
+    }
+
+    public class RenderableEmbedTextClass
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string FontFamily = "Arial";
+
+        [JsonProperty(Required = Required.Always)]
+        public int X = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Y = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int Size = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public int MaxCharacters = 0;
+
+        [JsonProperty(Required = Required.Always)]
+        public string Color = null;
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public bool Bold = false;
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public bool Italic = false;
+
+        [JsonProperty(Required = Required.Always)]
+        public string Text = null;
+    }
+}
---
+++ RenWeb/Embedding.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+
+namespace RenWeb
+{
+    public class Embedding
+    {
+        public static MemoryStream RenderImage(ImageFormat Format, RenderableEmbedClass EmbedData)
+        {
+            Bitmap bmp = new Bitmap(EmbedData.Width, EmbedData.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            //Drawing background first.
+            Image Background = Image.FromFile(Main.RootHTTPFolder + "\\" + EmbedData.BackgroundFile);
+            g.DrawImage(Background, 0, 0);
+            Background.Dispose();
+
+            //Iterating images
+            if (EmbedData.Images != null)
+            {
+                foreach (RenderableEmbedImageClass Img in EmbedData.Images)
+                {
+                    Image File = Image.FromFile(Main.RootHTTPFolder + "\\" + Img.Source);
+                    g.DrawImage(File, Img.X, Img.Y, Img.Width, Img.Height);
+                    File.Dispose();
+                }
+            }
+
+            if(EmbedData.Texts != null)
+            {
+                foreach(RenderableEmbedTextClass Text in EmbedData.Texts)
+                {
+                    FontStyle Style = FontStyle.Regular;
+                    if(Text.Bold && Text.Italic)
+                    {
+                        Style = FontStyle.Bold | FontStyle.Italic;
+                    }
+                    else if(Text.Bold)
+                    {
+                        Style = FontStyle.Bold;
+                    }
+                    else if(Text.Italic)
+                    {
+                        Style = FontStyle.Italic;
+                    }
+
+                    FontFamily Family = new FontFamily(Text.FontFamily);
+                    Font Font = new Font(Family, (float)Text.Size, Style, GraphicsUnit.Pixel);
+                    string Formatted = WebServer.ProcessHTML(Text.Text);
+                    if (Formatted.Length > Text.MaxCharacters)
+                         Formatted = Formatted.Substring(0, Text.MaxCharacters) + "...";
+                    g.DrawString(Formatted, Font, new SolidBrush(HexToColor(Text.Color)), Text.X, Text.Y);
+                }
+            }
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, Format);
+            return ms;
+        }
+
+        public static Color HexToColor(string Hex)
+        {
+            if(Hex[0] == '#')
+                Hex.Remove(0, 1);
+
+            if(Hex.Length == 6)
+            {
+                int R = Convert.ToInt32(Hex.Substring(0, 2), 16);
+                int G = Convert.ToInt32(Hex.Substring(2, 2), 16);
+                int B = Convert.ToInt32(Hex.Substring(4, 2), 16);
+                return Color.FromArgb(255, R, G, B);
+            }
+            return Color.White;
+        }
+    }
+}
